6a76c76b8714013aceeb1b5344bc6988b942a859
Anton <Anton@DESKTOP-6RE5PBQ>
2023-12-26 00:49:28 +0200

    Добавьте файлы проекта.

---
+++ ClassLibrary/Class1.cs
@@ -0,0 +1,30 @@
+﻿namespace ClassLibrary
+{
+    public class Matrix
+    {
+        public static string GetRowWithMaxSum (int [,] matrix)
+        {
+            int maxSum = 0;
+            int maxSumRow = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int rowSum = 0;
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    rowSum += matrix[i, j];
+                }
+                if (rowSum > maxSum)
+                {
+                    maxSum = rowSum;
+                    maxSumRow = i;
+                }
+            }
+            string vector = string.Empty;
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                vector += matrix[maxSumRow, j] + " ";
+            }
+            return vector.Trim();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Lab 6.2 базовый уровень
+using ClassLibrary;
+try
+{
+    int[,] matrix = new int[7, 5];
+    Random random = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = random.Next(0, 10);
+            Console.Write(matrix[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+    string vector = Matrix.GetRowWithMaxSum(matrix);
+    Console.WriteLine($"Ответ: {vector}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
